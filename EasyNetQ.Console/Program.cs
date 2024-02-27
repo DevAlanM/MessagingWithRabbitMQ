ef31cacf471cb4dc8da8ff5aa2a31055c49ce8c3
alanm <alanmartins789@gmail.com>
2024-02-26 22:29:32 -0300

    feat: My first project using the EasyNetQ library was added.

---
+++ EasyNetQ.Console/Program.cs
@@ -0,0 +1,42 @@
+﻿using EasyNetQ;
+using MessagingEvents.Shared.Models;
+using Newtonsoft.Json;
+
+const string EXCHANGE = "my-rabbitmq";
+const string QUEUE = "person-created";
+const string ROUTING_KEY = "hr.person-created";
+
+var person = new Person("Alan Martins", "123.456.789-10", new DateTime(2001, 10, 30));
+
+var bus = RabbitHutch.CreateBus("host=localhost");
+
+#region Advanced Publish Messaging
+
+var advanced = bus.Advanced;
+
+var exchange = advanced.ExchangeDeclare(EXCHANGE, type: "topic");
+var queue = advanced.QueueDeclare(QUEUE);
+
+advanced.Publish(exchange, ROUTING_KEY, true, new Message<Person>(person));
+
+advanced.Consume<Person>(queue, (msg, info) =>
+{
+    var json = JsonConvert.SerializeObject(msg.Body);
+    Console.WriteLine(json);
+});
+
+#endregion
+
+#region Simple Publish Messaging
+
+//await bus.PubSub.PublishAsync(person);
+
+//await bus.PubSub.SubscribeAsync<Person>("hr", msg =>
+//{
+//    var json = JsonConvert.SerializeObject(msg);
+//    Console.WriteLine(json);
+//});
+
+#endregion
+
+Console.ReadLine();
